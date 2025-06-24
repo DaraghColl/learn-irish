6d62035095d41e761eecadde23ae35b1d9751e1f
Daragh Coll <daraghcoll@gmail.com>
2025-06-24 18:08:20 +0100

    feat: update phrases

--- src/components/day-learning/day-learning.tsx
+++ src/components/day-learning/day-learning.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ChevronRight, ChevronDown } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
 import type { DayLearning as DayLearningI } from '@models/models';
 import { TopicItem } from '@/components/topic-item/topic-item';
 
@@ -13,16 +13,18 @@ const DayLearning = (props: DayLearningProps) => {
   const [isDayOpen, setIsDayOpen] = useState((day - 1) % 7 === 0 ? true : false);
 
   return (
-    <div className={`rounded-md border-1 border-gray-100 bg-gray-50`}>
+    <div className="rounded-md border-1 border-gray-100 bg-gray-50">
       <button
-        className="flex w-full cursor-pointer justify-between gap-4 rounded-t-md p-4"
+        className="flex w-full cursor-pointer items-center justify-between gap-4 rounded-t-md p-4"
         onClick={() => setIsDayOpen(!isDayOpen)}
       >
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4">
           <h3 className="text-nowrap">Day {day}</h3>
           <h4 className="text-left font-semibold">{topic}</h4>
         </div>
-        {!isDayOpen ? <ChevronRight /> : <ChevronDown />}
+        <ChevronRight
+          className={`${isDayOpen ? 'rotate-90' : ''} transition-all duration-300 ease-in-out`}
+        />
       </button>
 
       {isDayOpen && (

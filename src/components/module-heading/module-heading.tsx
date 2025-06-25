87b6ed0803387c4f646d2cabdb32e1c065d3caca
Daragh Coll <daraghcoll@gmail.com>
2025-06-25 22:45:25 +0100

    fix: module heading allignment

--- src/components/module-heading/module-heading.tsx
+++ src/components/module-heading/module-heading.tsx
@@ -7,11 +7,9 @@ const ModuleHeading = (props: ModuleHeadingProps) => {
   const { title, description } = props;
 
   return (
-    <div className="mb-10">
-      <h1 className="mb-4 text-2xl font-bold tracking-wide text-gray-950 lg:text-center lg:text-4xl">
-        {title}
-      </h1>
-      <h2 className="text-sm text-gray-500 lg:text-lg">{description}</h2>
+    <div className="mb-10 flex max-w-4xl flex-col items-center">
+      <h1 className="mb-4 text-2xl font-bold tracking-wide text-gray-950 lg:text-4xl">{title}</h1>
+      <h2 className="text-gray-500 lg:text-lg">{description}</h2>
     </div>
   );
 };
